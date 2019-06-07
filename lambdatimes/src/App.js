8df2e4f2fda9bd32c22cc6e13e1881980135d3df
Noble Obioma <nobioma1@gmail.com>
2019-06-07 10:12:29 +0100

    Convert App Component to Class Component to hold state modal current user. - import HOC and pass on Login and Content as arguments setting return value to a variable - On mount check if user is in the localstorage and set the user to state - toggle method passed down into Login to open or close modal - logout method to clear local storage - and render the return value of the HOC passing state and the toogle method

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,15 +2,46 @@ import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
+import Login from './components/Login';
+import withAuthenticate from './components/auth/withAuthenticate';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+const AuthComponent = withAuthenticate(Login)(Content);
+
+class App extends Component {
+  state = {
+    modal: true,
+    currentUser: ''
+  };
+
+  componentDidMount() {
+    let currentUser = localStorage.getItem('react-lambda-times-noble');
+    this.setState({ currentUser });
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  };
+
+  logout = () => {
+    localStorage.removeItem('react-lambda-times-noble');
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <TopBar
+          currentUser={this.state.currentUser}
+          toggle={this.toggle}
+          logout={this.logout}
+        />
+        <Header />
+        <AuthComponent {...this.state} toggle={this.toggle} />
+      </div>
+    );
+  }
 }
 
 export default App;
