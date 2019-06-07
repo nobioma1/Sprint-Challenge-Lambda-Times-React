9b95e6b8a8a4c2714f53731a81c97bf3f36854dd
Noble Obioma <nobioma1@gmail.com>
2019-06-07 08:23:15 +0100

    filterCards Method to conditionally return cards depending on the value of selected in the state

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -27,19 +27,12 @@ export default class Content extends Component {
   };
 
   filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
-    return this.state.cards;
+    const currentOption = this.state.selected;
+    if (currentOption === 'all') {
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter(card => card.tab === currentOption);
+    }
   };
 
   render() {
